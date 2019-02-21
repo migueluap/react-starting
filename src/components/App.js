41db6aca893c69bfc9a9dc82cf604a41233e143d
Miguel <migueluap@hotmail.com>
2019-02-21 14:56:54 -0300

    add some code

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component{
     render(){
         return (
             <div>
-                <Contador />
+                <PlacarContainer />
             </div>
         );
     }
